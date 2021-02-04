e0f6e829f2fcb94ba1820aa07a781c60f023d056
oinsua <ossany.insua1982@gmail.com>
2021-02-04 16:42:45 -0500

    update package2

--- server-chat/index.js
+++ server-chat/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
-const cors = require('cors');
+//const cors = require('cors');
 
 const port = process.env.PORT || 4000; //Se define el puerto a traves del cual se brinda el servicio
 const router = require('./route/router'); //Se importan las rutas
-app.use(cors()); 
+//app.use(cors()); 
 
 const app = express(); //Crea el objeto app a traves de express
 const server = http.createServer(app); //Crear un server a traves de http para establecer la coneccion websocket
